359300c603fbdabb669dcfc77a2f2672178a800c
Takeshi Iwamoto <rock.book.ganpon@outlook.com>
2016-05-22 14:27:40 +0900

    プロジェクト ファイルを追加します。

---
+++ lightbard/ViewModels/CommandViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lightbard.ViewModels
+{
+  public class CommandViewModel : Common.BindableBase
+  {
+    private Models.CommandModel Model { get; } = Models.CommandModel.Instance;
+
+
+
+  }
+}
